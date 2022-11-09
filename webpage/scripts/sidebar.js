dc8045bf16722d124cde31ab2c7aff36f95ff528
MiepHD <63968466+MiepHD@users.noreply.github.com>
2022-11-09 19:08:36 +0100

    Fixed sidebar don't resize

--- webpage/scripts/sidebar.js
+++ webpage/scripts/sidebar.js
@@ -6,24 +6,30 @@ function toggleSidebar() {
 		showSidebar();
 	}
 }
-function showSidebar() {
+function resetSidebar() {
 	if (!shown) {
-		width = document.getElementById("sidebar").offsetWidth;
-		left = document.querySelector("body").offsetWidth - width;
-		sidebar = document.getElementById("sidebar");
-		sidebar.style.left = `${left}px`;
-		sidebar.style.paddingTop = `${width * 1.1}px`;
-		document.getElementById("menu").style.transform = "rotate(90deg)";
-		shown = true;
+		hideSidebar();
+	} else {
+		showSidebar();
 	}
 }
+function showSidebar() {
+	width = document.getElementById("sidebar").offsetWidth;
+	left = document.querySelector("body").offsetWidth - width;
+	sidebar = document.getElementById("sidebar");
+	sidebar.style.left = `${left}px`;
+	sidebar.style.paddingTop = `${width * 1.1}px`;
+	document.getElementById("menu").style.transform = "rotate(90deg)";
+	shown = true;
+}
 function hideSidebar() {
-	if (shown) {
-		document.getElementById("sidebar").style.left = "100%";
-		document.getElementById("menu").style.transform = "rotate(0deg)";
-		shown = false;
-	}
+	document.getElementById("sidebar").style.left = "100%";
+	document.getElementById("menu").style.transform = "rotate(0deg)";
+	shown = false;
 }
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById("menu").addEventListener("click", toggleSidebar);
+		window.addEventListener('resize', function() {
+			resetSidebar()
+		});
 }, false);
