79634ae94a65673bb2b80d305e2015f9bfb02dd0
MiepHD <63968466+MiepHD@users.noreply.github.com>
2022-11-10 14:55:49 +0100

    Removed unnecessary variable

--- webpage/scripts/add-icon.js
+++ webpage/scripts/add-icon.js
@@ -1,9 +1,9 @@
 //Adds the add icon to every category
 document.addEventListener('DOMContentLoaded', function() {
-  addobject = document.getElementById("add-icon");
-  lists = document.querySelectorAll(".tiles");
-  for (list of lists) {
-    newadd = $(addobject).clone().appendTo(list);
+  var addobject = document.getElementById("add-icon");
+  var lists = document.querySelectorAll(".tiles");
+  for (var list of lists) {
+    $(addobject).clone().appendTo(list);
   }
   //Remove original icon
   document.getElementById("add-icon").remove();
