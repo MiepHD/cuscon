5e026600cb2743797a93f08c800cc8815f693122
MiepHD <63968466+MiepHD@users.noreply.github.com>
2022-12-06 14:39:00 +0100

    Optimized look of cards

--- webpage/scripts/cards.js
+++ webpage/scripts/cards.js
@@ -2,10 +2,13 @@ class Cards {
   build() {
     for (const card of document.querySelectorAll(".card") ) {
       const content = card.innerHTML, //saves the content temporarly
-        div = document.createElement("div");
+        div = document.createElement("div"),
+        translationid = card.getAttribute("data-translation-id");
       card.innerHTML = ""; //Removes existing content
+      card.removeAttribute("data-translation-id");
       div.classList.add("card-content");
       div.innerHTML = content; //adds content back
+      div.setAttribute("data-translation-id", translationid);
       card.appendChild(div);
     };
   }
