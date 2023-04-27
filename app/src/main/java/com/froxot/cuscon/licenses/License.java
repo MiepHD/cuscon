e0208f7e076f996ae7c7261d50bb487354ca2039
MiepHD <63968466+MiepHD@users.noreply.github.com>
2023-04-27 16:15:20 +0200

    Removed permission and changed config

--- app/src/main/java/com/froxot/cuscon/licenses/License.java
+++ app/src/main/java/com/froxot/cuscon/licenses/License.java
@@ -1,7 +1,5 @@
 package com.froxot.cuscon.licenses;
 
-import candybar.lib.items.InAppBilling;
-
 public class License {
     private static final boolean ENABLE_LICENSE_CHECKER = false;
     private static final byte[] SALT = new byte[]{};
