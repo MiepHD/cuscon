a5b6b915d1daed15311bce252ff3beb6af118d49
MiepHD <63968466+MiepHD@users.noreply.github.com>
2023-06-15 21:20:45 +0200

    Changed to work with fossBuild

---
+++ play/activities/MainActivity.java
@@ -0,0 +1,21 @@
+package com.froxot.cuscon.activities;
+
+import androidx.annotation.NonNull;
+
+import candybar.lib.activities.CandyBarMainActivity;
+
+import com.froxot.cuscon.licenses.License;
+
+public class MainActivity extends CandyBarMainActivity {
+
+    @NonNull
+    @Override
+    public ActivityConfiguration onInit() {
+        return new ActivityConfiguration()
+                .setLicenseCheckerEnabled(License.isLicenseCheckerEnabled())
+                .setLicenseKey(License.getLicenseKey())
+                .setRandomString(License.getRandomString())
+                .setDonationProductsId(License.getDonationProductsId())
+                .setPremiumRequestProducts(License.getPremiumRequestProductsId(), License.getPremiumRequestProductsCount());
+    }
+}
---
+++ play/services/ClockWidget.java
@@ -0,0 +1,5 @@
+package com.froxot.cuscon.services;
+
+import candybar.lib.services.CandyBarWidgetService;
+
+public class ClockWidget extends CandyBarWidgetService {}
