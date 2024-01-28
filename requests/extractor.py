84a88bfccf75af69c0b4ef314c5b1f685b8f9980
MiepHD <63968466+MiepHD@users.noreply.github.com>
2024-01-28 12:48:16 +0100

    Added Device Info, Muslim Pro, Parental controls, Runmefit, Sound notifications, Space Intern, Geometry Dash

--- requests/extractor.py
+++ requests/extractor.py
@@ -19,17 +19,18 @@ for elem in sys.argv:
                     changelog = title
                 else:
                     changelog = changelog + ", " + title
-                appfilter = appfilter + line
+                appfilter = appfilter + re.sub(r"[\\\t]+", "    ", line)
             title = line
     with open("appmap.xml", encoding="utf-8") as f:
         for line in f.readlines():
             if (elem in line):
-                appmap = appmap + line
+                appmap = appmap + re.sub(r"[\\\t]+", "    ", line)
     with open("theme_resources.xml", encoding="utf-8") as f:
         for line in f.readlines():
             if (elem in line):
-                    theme_resources = theme_resources + line
+                    theme_resources = theme_resources +re.sub(r"[\\\t]+", "    ", line)
 print(changelog)
 print(appfilter)
 print(appmap)
+print(re.sub(' component=".*?"', "", appfilter))
 print(theme_resources)
