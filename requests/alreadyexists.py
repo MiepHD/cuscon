9b185fb2d28216e9d21560f8175d7962ef0595e8
MiepHD <63968466+MiepHD@users.noreply.github.com>
2024-01-28 13:25:52 +0100

    Added ADM, BHIM, Chalo, Coursera, My OPPO, OpenBooksDownloader, Rapido, Swayam, Unsplash, Video Compressor Updated Videos

---
+++ requests/alreadyexists.py
@@ -0,0 +1,12 @@
+import sys
+import re
+appfilter = ""
+with open("../../app/src/main/res/xml/appfilter.xml", encoding="utf-8") as f1:
+    already = f1.read()
+    with open("appfilter.xml", encoding="utf-8") as f:
+        for line in f.readlines():
+            if ("item" in line):
+                line = re.sub(r"[\\\t]+", "", line)
+                if (line in already):
+                    appfilter = appfilter + line
+print(appfilter)
