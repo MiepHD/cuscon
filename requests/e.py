1c408aa9c42053eaa0f261b84dea801639606490
MiepHD <63968466+MiepHD@users.noreply.github.com>
2024-02-02 17:57:36 +0100

    Added Deezer, Firewall, FreeTube, hagebau, Inoreader, Music Player, mytonies, Neato, Personio, Pocket, Rossmann, soundcore, Tapo, TK, Trello, use

--- requests/e.py
+++ requests/e.py
@@ -3,6 +3,7 @@ import re
 from os.path import exists
 
 appfilter = ""
+conflict = ""
 with open("../../app/src/main/res/xml/appfilter.xml", encoding="utf-8") as f1:
     already = f1.read()
     with open("appfilter.xml", encoding="utf-8") as f:
@@ -10,8 +11,12 @@ with open("../../app/src/main/res/xml/appfilter.xml", encoding="utf-8") as f1:
             if ("item" in line):
                 line = re.sub(r"[\\\t]+", "", line)
                 find = re.sub(r'drawable=".*?\n', "", line)
-                if (find in already):
-                    name = re.search('(?<=e=").*?(?=")', line).group()
-                    if (exists(name + ".png")):
+                name = re.search('(?<=e=").*?(?=")', line).group()
+                if (exists(name + ".png")):
+                    if (find in already):
                         appfilter = appfilter + name + '\n'
+                    if (exists("../../app/src/main/res/drawable-nodpi/" + name + ".png")):
+                        conflict = conflict + name + '\n'
 print(appfilter)
+print("Conflicts:")
+print(conflict)
