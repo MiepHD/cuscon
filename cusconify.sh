213fd07ca4e7934f5eeef35cd3c503ebaca23bf1
Jeremiah Miller <jmil@tuta.io>
2024-02-03 16:21:12 -0700

    Break up magick commands to generate icons at various stages of processing

--- cusconify.sh
+++ cusconify.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # 1st parameter: filename
-# 2nd optional parameter: fuzz percentage
+# 2nd optional parameter: fuzz percentage for background
+# 3rd optional parameter: fuzz percentage for trimming
 
 filename="$1"
 new_file="new_$filename"
@@ -26,10 +27,20 @@ echo
 print_background $filename
 
 echo
+echo "Remove background"
+magick $new_file -fill none -fuzz ${2:-25}% -draw 'color 0,0 floodfill' "no_bg_$new_file"
+
+echo "Trim transparent border"
+magick "no_bg_$new_file" -fuzz ${3:-${2:-75}}% -trim "trimmed_$new_file"
+
 # resize preserves the aspect ratio, so the longest edge is now 450px
 # https://imagemagick.org/Usage/resize/
-echo "Remove the background, trim the transparent border, and resize the image so the longest side is 450px, and add transparent border such that the image is 512x512 with the icon in the center"
-magick $new_file -fill none -fuzz ${2:-50}% -draw 'color 0,0 floodfill' -trim -resize 450x450 -background transparent -gravity center -extent 512x512 $new_file
+echo "Resize trimmed image so longest side is 450px"
+magick "trimmed_$new_file" -resize 450x450 "450_$new_file"
+
+echo "Put 450px trimmed image in center of 512x512 transparent image"
+magick "450_$new_file" -background transparent -gravity center -extent 512x512 "aio_$new_file"
+
 
 echo "Generate icon with black borders"
-magick $new_file -bordercolor none -border 12 -background black -alpha background -channel A -blur 12x12 -level 0,5% "bordered_$new_file"
+magick "aio_$new_file" -bordercolor none -border 12 -background black -alpha background -channel A -blur 12x12 -level 0,5% "bordered_$new_file"
