db76db095690abc5fa2f885bdb481c8014136229
Jeremiah Miller <jmil@tuta.io>
2024-02-03 13:09:37 -0700

    Generate icon with black borders with script

--- cusconify.sh
+++ cusconify.sh
@@ -29,3 +29,5 @@ echo
 echo "Remove the background, trim the transparent border, and resize the image so the longest side is 450px, and add transparent border such that the image is 512x512 with the icon in the center"
 magick $new_file -fill none -fuzz 50% -draw 'color 0,0 floodfill' -trim -resize 450x450 -background transparent -gravity center -extent 512x512 $new_file
 
+echo "Generate icon with black borders"
+magick $new_file -bordercolor none -border 12 -background black -alpha background -channel A -blur 12x12 -level 0,5% "bordered_$new_file"
