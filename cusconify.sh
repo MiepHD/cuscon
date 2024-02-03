1f0fad0e5ff738a17886a0f429dc25a5ecef9d93
Jeremiah Miller <jmil@tuta.io>
2024-02-03 12:29:30 -0700

    Add script to convert requested icons to make them easier to cusconify

---
+++ cusconify.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+filename="$1"
+new_file="new_$filename"
+#background_color=${2:-$(command_to_get_bg)}
+
+print_background() {
+    echo "Colors around the edge of $1 to get the background color"
+    for x in 0 256 512
+    do
+        for y in 0 256 512
+        do
+            if [ $x != 256 ] || [ $y != 256 ]; then
+                magick $1 -format "Color at ($x,$y) %[pixel:u.p{$x,$y}]\n" info:
+            fi
+        done
+    done
+}
+
+echo "Resize $filename to 512x512"
+magick $filename -resize 512x512! $new_file
+
+echo
+print_background $filename
+
+echo
+# resize preserves the aspect ratio, so the longest edge is now 450px
+# https://imagemagick.org/Usage/resize/
+echo "Remove the background, trim the transparent border, and resize the image so the longest side is 450px, and add transparent border such that the image is 512x512 with the icon in the center"
+magick $new_file -fill none -fuzz 50% -draw 'color 0,0 floodfill' -trim -resize 450x450 -background transparent -gravity center -extent 512x512 $new_file
+
