37bea87ea95ed62643c4fdc8f18698b943d60c41
Jeremiah Miller <jmil@tuta.io>
2024-02-04 07:07:32 -0700

    Update cusconify.sh to put intermediate images into a temp folder, trim blank space that might be around the edge of the initial icon

--- requests/cusconify.sh
+++ requests/cusconify.sh
@@ -9,6 +9,7 @@
 
 filename="$1"
 new_file="new_$filename"
+temp_dir="cusconify_temp"
 
 print_background() {
     echo "Colors around the edge of $1 to get the background color"
@@ -23,8 +24,10 @@ print_background() {
     done
 }
 
+mkdir -p $temp_dir
+
 echo "Resize $filename to 512x512"
-magick $filename -resize 512x512! $new_file
+magick $filename -fuzz 20% -trim -resize 512x512! $temp_dir/$new_file
 
 echo
 print_background $filename
@@ -32,22 +35,22 @@ print_background $filename
 echo
 echo "Remove background"
 if [ "$6" = "replace" ]; then
-    magick $new_file -fill none -fuzz ${4:-25}% -draw "color ${2:-0},${3:-0} replace" "no_bg_$new_file"
+    magick $temp_dir/$new_file -fill none -fuzz ${4:-25}% -draw "color ${2:-256},${3:-0} replace" "$temp_dir/no_bg_$new_file"
 else
-    magick $new_file -fill none -fuzz ${4:-25}% -draw "color ${2:-0},${3:-0} floodfill" "no_bg_$new_file"
+    magick $temp_dir/$new_file -fill none -fuzz ${4:-25}% -draw "color ${2:-0},${3:-0} floodfill" "$temp_dir/no_bg_$new_file"
 fi
 
 echo "Trim transparent border"
-magick "no_bg_$new_file" -fuzz ${5:-75}% -trim "trimmed_$new_file"
+magick "$temp_dir/no_bg_$new_file" -fuzz ${5:-75}% -trim "$temp_dir/trimmed_$new_file"
 
 # resize preserves the aspect ratio, so the longest edge is now 450px
 # https://imagemagick.org/Usage/resize/
 echo "Resize trimmed image so longest side is 450px"
-magick "trimmed_$new_file" -resize 450x450 "450_$new_file"
+magick "$temp_dir/trimmed_$new_file" -resize 450x450 "$temp_dir/450_$new_file"
 
 echo "Put 450px trimmed image in center of 512x512 transparent image"
-magick "450_$new_file" -background transparent -gravity center -extent 512x512 "aio_$new_file"
+magick "$temp_dir/450_$new_file" -background transparent -gravity center -extent 512x512 "$temp_dir/aio_$new_file"
 
 
 echo "Generate icon with black borders"
-magick "aio_$new_file" -bordercolor none -border 12 -background black -alpha background -channel A -blur 12x12 -level 0,5% -resize 512x512! "bordered_$new_file"
+magick "$temp_dir/aio_$new_file" -bordercolor none -border 12 -background black -alpha background -channel A -blur 12x12 -level 0,5% -resize 512x512! "$new_file"
