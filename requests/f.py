7110e79f755d21ca27b3251598929239ebb6f44c
MiepHD <63968466+MiepHD@users.noreply.github.com>
2024-02-04 17:00:13 +0100

    Fixed find script

--- requests/f.py
+++ requests/f.py
@@ -8,7 +8,7 @@ appfilter = ""
 appmap = ""
 theme_resources = ""
 
-if (len(sys.argv) == 0):
+if (len(sys.argv) == 1):
     params = os.listdir("get/")
     params = [e.replace(".png", "") for e in params]
 else:
