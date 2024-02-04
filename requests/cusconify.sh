81746cbffbc3f631c74cbbb6936d1283bca48dec
Jeremiah Miller <jmil@tuta.io>
2024-02-04 07:22:07 -0700

    Add +repage to commands 'https://www.legacy.imagemagick.org/discourse-server/viewtopic.php?t=35826'

--- requests/cusconify.sh
+++ requests/cusconify.sh
@@ -27,7 +27,7 @@ print_background() {
 mkdir -p $temp_dir
 
 echo "Resize $filename to 512x512"
-magick $filename -fuzz 20% -trim -resize 512x512! $temp_dir/$new_file
+magick $filename -fuzz 20% -trim -resize 512x512 +repage $temp_dir/$new_file
 
 echo
 print_background $filename
@@ -35,25 +35,25 @@ print_background $filename
 echo
 echo "Remove background"
 if [ "$6" = "replace" ]; then
-    magick $temp_dir/$new_file -fill none -fuzz ${4:-25}% -draw "color ${2:-256},${3:-0} replace" "$temp_dir/no_bg_$new_file"
+    magick $temp_dir/$new_file -fill none -fuzz ${4:-25}% -draw "color ${2:-256},${3:-0} replace" +repage "$temp_dir/no_bg_$new_file"
 else
-    magick $temp_dir/$new_file -fill none -fuzz ${4:-25}% -draw "color ${2:-0},${3:-0} floodfill" "$temp_dir/no_bg_$new_file"
+    magick $temp_dir/$new_file -fill none -fuzz ${4:-25}% -draw "color ${2:-0},${3:-0} floodfill" +repage "$temp_dir/no_bg_$new_file"
 fi
 
 echo "Trim transparent border"
-magick "$temp_dir/no_bg_$new_file" -fuzz ${5:-75}% -trim "$temp_dir/trimmed_$new_file"
+magick "$temp_dir/no_bg_$new_file" -fuzz ${5:-75}% -trim +repage "$temp_dir/trimmed_$new_file"
 
 # resize preserves the aspect ratio, so the longest edge is now 450px
 # https://imagemagick.org/Usage/resize/
 echo "Resize trimmed image so longest side is 450px"
-magick "$temp_dir/trimmed_$new_file" -resize 450x450 "$temp_dir/450_$new_file"
+magick "$temp_dir/trimmed_$new_file" -resize 450x450 +repage "$temp_dir/450_$new_file"
 
 echo "Put 450px trimmed image in center of 512x512 transparent image"
-magick "$temp_dir/450_$new_file" -background transparent -gravity center -extent 512x512 "$temp_dir/aio_$new_file"
+magick "$temp_dir/450_$new_file" -background transparent -gravity center -extent 512x512 +repage "$temp_dir/aio_$new_file"
 
 
 echo "Generate icon with black borders"
-magick "$temp_dir/aio_$new_file" -bordercolor none -border 12 -background black -alpha background -channel A -blur 12x12 -level 0,5% -resize 512x512 "$temp_dir/bordered_$new_file"
+magick "$temp_dir/aio_$new_file" -bordercolor none -border 12 -background black -alpha background -channel A -blur 12x12 -level 0,5% -resize 512x512 +repage "$temp_dir/bordered_$new_file"
 
 echo "Make sure final icon is 512x512"
-magick "$temp_dir/bordered_$new_file" -background transparent -gravity center -extent 512x512 "$new_file"
+magick "$temp_dir/bordered_$new_file" -background transparent -gravity center -extent 512x512 +repage "$new_file"
