0d5940b2a720cd9d9f071ccfb2bee56c074c3aed
MiepHD <63968466+MiepHD@users.noreply.github.com>
2024-04-13 14:20:03 +0200

    Updated File Manager

--- requests/e.py
+++ requests/e.py
@@ -9,7 +9,7 @@ with open("../../app/src/main/res/xml/appfilter.xml", encoding="utf-8") as f1:
     already = f1.read()
     with open("appfilter.xml", encoding="utf-8") as f:
         for line in f.readlines():
-            if ("item" in line):
+            if ("<item" in line):
                 line = re.sub(r"[\\\t]+", "", line)
                 find = re.sub(r'drawable=".*?\n', "", line)
                 name = re.search('(?<=e=").*?(?=")', line).group()
