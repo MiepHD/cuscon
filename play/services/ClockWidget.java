81318bcb35d36e5f71d59b1ac2ece41f22af966c
MiepHD <63968466+MiepHD@users.noreply.github.com>
2024-05-04 20:18:07 +0200

    Updated store data

--- play/services/ClockWidget.java
+++
@@ -1,5 +0,0 @@
-package com.froxot.cuscon.services;
-
-import candybar.lib.services.CandyBarWidgetService;
-
-public class ClockWidget extends CandyBarWidgetService {}
--- play/services/MuzeiService.java
+++
@@ -1,5 +0,0 @@
-package com.froxot.cuscon.services;
-
-import candybar.lib.services.CandyBarMuzeiService;
-
-public class MuzeiService extends CandyBarMuzeiService {}
